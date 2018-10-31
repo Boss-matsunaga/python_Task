e6517c5e3fc169da340a3c4f0eeafde0168b6a3a
Boss-matsunaga <32508729+Boss-matsunaga@users.noreply.github.com>
2018-10-31 22:15:46 +0900

    Add files via upload

--- 1chapter/04.py
+++ 1chapter/04.py
@@ -1,2 +1,23 @@
 text = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
 
+dic = {}
+count = 1
+li = [1, 5, 6, 7, 8, 9, 15, 16, 19]
+flag = 1
+
+for i in text:
+    mo = ""
+    if i == " " or i == ".":
+        count += 1
+        flag = 1
+    elif count in li and flag == 1:
+        flag = 0
+        mo += i
+        # mo += i+1
+        dic[mo] = count
+    elif flag == 1:
+        flag = 0
+        mo += i
+        dic[mo] = count
+
+print(dic)
