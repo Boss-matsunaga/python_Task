0734f17f22f31c50c49dada7c6bfb8af47f48428
shunmame <32508729+Boss-matsunaga@users.noreply.github.com>
2018-11-01 23:00:30 +0900

    Add files via upload

--- 1chapter/05.py
+++ 1chapter/05.py
@@ -1,11 +1,24 @@
-import ngram
-
 text = "I am an NLPer"
 
-def ngram(n):
-    text2 = ngram.NGram(n)
-    for i in index.ngrams(index.pad(text)):
-        print(i)
+def tan_ngram(n):
+    text2 = text.replace(" ", "")
+    print(text2)
+    for i in range(0, len(text2), n-1):
+        result1 = ""
+        result1 += text2[i:i+n]
+        print(result1)
+
+def moz_ngram(n):
+    result = ""
+    text2 = text.split(" ")
+    print(text2)
+    for i in range(0, len(text2), n-1):
+        result2 = []
+        result2 += text2[i:i+n]
+        for j in result2:
+            result += j
+        print(j)
 
 if __name__ == '__main__':
-    ngram(4)
+    tan_ngram(3)
+    moz_ngram(2)
