bfd8c4be7e922aee4afc784264f50d62fe36148b
松永俊輔 <matsunagashunsuke@matsunagashunsukenoMacBook-ea.local>
2018-11-02 22:20:44 +0900

    2

--- 1chapter/06.py
+++ 1chapter/06.py
@@ -1,10 +1,33 @@
-result = ""
-text2 = text.split(" ")
-print(text2)
-for i in range(0, len(text2), n-1):
-    result2 = []
-    result2 += text2[i:i+n]
+text = "paraparaparadise"
+text2 = "paragraph"
+n = 2
 
-    for j in result2:
-        result += j
-    print(result)
+X = []
+Y = []
+
+for i in range(0, len(text), n-1):
+    result1 = ""
+    result1 += text[i:i+n]
+    X.append(result1)
+
+for i in range(0, len(text2), n-1):
+    result2 = ""
+    result2 += text2[i:i+n]
+    Y.append(result2)
+
+if 'se' in X or 'se' in Y:
+    print("ある")
+
+# print(X)
+# print(Y)
+
+for i in X:
+    for j in Y:
+        ii = set(i)
+        jj = set(j)
+        print("和集合")
+        print(ii | jj)
+        print("積集合")
+        print(ii & jj)
+        print("差集合")
+        print(ii - jj)
