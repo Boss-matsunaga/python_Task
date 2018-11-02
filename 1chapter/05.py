cca8ccccb3c0b75459b550461d4e0dd95f6ff93b
松永俊輔 <matsunagashunsuke@matsunagashunsukenoMacBook-ea.local>
2018-11-02 22:06:13 +0900

    1

--- 1chapter/05.py
+++ 1chapter/05.py
@@ -1,24 +1,21 @@
 text = "I am an NLPer"
 
-def tan_ngram(n):
+def moz_ngram(n):
     text2 = text.replace(" ", "")
-    print(text2)
     for i in range(0, len(text2), n-1):
         result1 = ""
         result1 += text2[i:i+n]
         print(result1)
 
-def moz_ngram(n):
-    result = ""
+def tan_ngram(n):
     text2 = text.split(" ")
-    print(text2)
     for i in range(0, len(text2), n-1):
+        k = ""
         result2 = []
         result2 += text2[i:i+n]
-        for j in result2:
-            result += j
-        print(j)
+        k = ''.join(result2)
+        print(k)
 
 if __name__ == '__main__':
-    tan_ngram(3)
+    tan_ngram(2)
     moz_ngram(2)
--- 1chapter/06.py
+++ 1chapter/06.py
@@ -1,1 +1,10 @@
-print("aaaaaaaaaaaaa")
+result = ""
+text2 = text.split(" ")
+print(text2)
+for i in range(0, len(text2), n-1):
+    result2 = []
+    result2 += text2[i:i+n]
+
+    for j in result2:
+        result += j
+    print(result)
